cb9f198166d54d47ee30432230fffa0d105145ec
«SergioBertomeu» <sbertomeu1@iesebre.com>
2023-11-03 16:03:34 +0100

    WIP

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = [
+        'name' => 'Tasca1'
+    ];
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
+
+Route::get('/tasks', function () {
+    return view('tasks', [
+        'tasks' => []
+    ]);
+});
+
+Route::get('/users', function () {
+    return view('users');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
