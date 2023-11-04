e40c58571fb88c7dfe61326f3220861b0f21c00d
«SergioBertomeu» <sbertomeu1@iesebre.com>
2023-11-04 11:38:00 +0100

    Refactoritza la vista tasks per utilitzar Laravel Blade

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -47,25 +47,25 @@
 
 
 
-                                    <?php foreach ($tasks as $task):?>
+                                   @foreach ($tasks as $task)
                                     <tr>
                                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">L
-                                                <?=$task->id;?>
+                                                {{$task->id}}
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                <?=$task->title;?>
+                                                {{$task->title}}
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                <?=$task->description;?>
+                                                {{$task->description}}
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                <?=$task->completed;?>
+                                                {{$task->completed}}
                                         </td>
                                         <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                                             <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
                                         </td>
                                     </tr>
-                                    <?php endforeach; ?>
+                                    @endforeach
                                         <!-- More people...
 
                                      More people... -->
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,25 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
+
+    $task1 = new stdClass();
+    $task1->id = 1;
+    $task1->title = "Task 1";
+    $task1->description = "Bla bla bla";
+    $task1->completed = 1;
+
+    $task2 = new stdClass();
+    $task2->id = 2;
+    $task2->title = "Task 2";
+    $task2->description = "Bla bla bla";
+    $task2->completed = 1;
+
     $tasks = [
-        'name' => 'Tasca1'
+        $task1,
+        $task2
     ];
+
+
     return view('tasks', [
         'tasks' => $tasks
     ]);
