355819ba008477308b0b93ac784cf3c7b25eca20
«SergioBertomeu» <sbertomeu1@iesebre.com>
2023-11-04 19:21:46 +0100

    Tasca acabada PHP amb Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -30,15 +32,9 @@ Route::get('/', [TaskController::class, 'index']);
 
 Route::get('/tasks', [TaskController::class, 'index']);
 
-Route::get('/users', function () {
-    return view('users');
-});
+Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', [PagesController::class, 'contact']);
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [PagesController::class, 'about']);
 
